242baf0b9b4233defda095f0c59c105d885034f4
Mateusz <caputa.mateusz.pl@gmail.com>
2025-12-16 04:29:16 +0100

    chore: update app texts

--- app/page.js
+++ app/page.js
@@ -13,7 +13,8 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-[60vh] text-center">
       <h1 className="text-5xl font-extrabold text-gray-900 mb-6 tracking-tight">
-        Frontend Laboratory <span className="text-indigo-600">App</span>
+        Frameworki Frontendowe (LAB){" "}
+        <span className="text-indigo-600">App</span>
       </h1>
 
       <p className="text-xl text-gray-600 max-w-2xl mb-10">
